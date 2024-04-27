7cd3ced357e6bb4e4fe74579363423f171f1e102
nymaa <dorjderemnymdorj81@gmail.com>
2024-04-28 01:19:35 +0800

    nymaa

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -1,20 +1,3 @@
-// "use client";
-// import { useState } from "react";
-// import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
-
-// export default function Intro() {
-//   const position = { lat: 29, lng: 28 };
-//   const apiKey = import.meta.env.NEXT_PUBLIC_KEY_API;
-
-//   return (
-//     <APIProvider key={apiKey}>
-//       <div className="h-full w-full">
-//         <Map zoom={9} center={position}></Map>
-//       </div>
-//     </APIProvider>
-//   );
-// }
-
 import React from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
